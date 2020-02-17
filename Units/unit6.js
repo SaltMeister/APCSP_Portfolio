1048f17b207ca2bdd8944df02b9075293e9f7790
Simon Huang <huisimonliu@gmail.com>
2020-02-17 06:10:03 +0000

    added code for unit 6

---
+++ Units/unit6.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+    $("#chapter3Button").click(function()
+    {
+        window.location.href = "Units/unit3.html";
+    });
+    $("#chapter4Button").click(function()
+    {
+        window.location.href = "Units/unit4.html";
+    });
+    $("#chapter2Button").click(function()
+    {
+        window.location.href = "../index.html";
+    });
+});
