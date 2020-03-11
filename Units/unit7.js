51cad6ff7357f2a0e5f0741ef0bdf600a2b5eb9c
Simon Huang <huisimonliu@gmail.com>
2020-03-11 16:05:40 +0000

    almost complete unit 7

---
+++ Units/unit7.js
@@ -0,0 +1,19 @@
+$(document).ready(function()
+{
+    $("#chapter3Button").click(function()
+    {
+        window.location.href = "./unit3.html";
+    });
+    $("#chapter4Button").click(function()
+    {
+        window.location.href = "./unit4.html";
+    });
+    $("#chapter2Button").click(function()
+    {
+        window.location.href = "../index.html";
+    });
+    $("#chapter5Button").click(function()
+    {
+        window.location.href = "./unit5.html";
+    });
+});
